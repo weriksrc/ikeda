e3c3aea448f1ddb5ee4ecdb1cef1366c212546cc
weriks <ti15@viacometa.com.br>
2023-06-10 14:33:11 -0400

    refactor: importa modal

--- src/views/personal/users/components/UserCard.vue
+++ src/views/personal/users/components/UserCard.vue
@@ -1,5 +1,6 @@
 <template>
-  <v-card class="user-card">
+  <div>
+  <v-card class="user-card" @click="dialog = true">
     <v-img src="../../../../assets/img/user-card.png">
       <div class="user-information">
         <v-avatar color="#777777" size="60" class="mt-2">
@@ -12,11 +13,24 @@
       </div>
     </v-img>
   </v-card>
+  <ModalUser :dialog="dialog" :idUser="id" :name="name" @close="dialog = false" />
+  </div>
 </template>
 
 <script>
+import  ModalUser from "@/views/personal/users/components/ModalUser.vue";
 export default {
   props:["id","status","name"],
+
+  components:{
+    ModalUser
+  },
+
+  data(){
+    return{
+      dialog: false
+    }
+  },
 
   methods:{
     formatName(name){
@@ -26,6 +40,10 @@ export default {
 
     classAtivo(status){
       return status ? 'ma-0 mt-2 subtitle-2 font-weight-bold green--text' :'ma-0 mt-2 subtitle-2 font-weight-bold red--text'
+    },
+
+    teste(id){
+      console.log("id", id);
     }
   }
 }
