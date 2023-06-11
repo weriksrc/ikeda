4a24b4e34359ee6835bbe633f9af8e660aa518d7
weriks <ti15@viacometa.com.br>
2023-06-10 21:10:08 -0400

    feat: cria componente de informacao

---
+++ src/views/user/evolution/components/InformationCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card color="#232323" class="d-flex flex-column">
+    <v-card-title class="align-self-center card-text">{{ title }}</v-card-title>
+    <v-card-subtitle class="display-1 align-self-center card-text">{{ subtitle }}</v-card-subtitle>
+  </v-card>
+</template>
+
+<script>
+export default {
+
+  props:['title', 'subtitle'],
+
+  data(){
+    return{}
+  }
+}
+</script>
+
+<style>
+.card-text{
+  color: rgba(255, 255, 255, 0.3) !important;
+}
+</style>
