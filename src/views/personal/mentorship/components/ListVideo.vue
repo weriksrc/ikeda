dd74906fd16138bc6bb415379ad6ca340402da6f
weriks <ti15@viacometa.com.br>
2023-06-10 21:10:56 -0400

    refactor: adiciona informacao manual

--- src/views/personal/mentorship/components/ListVideo.vue
+++ src/views/personal/mentorship/components/ListVideo.vue
@@ -9,7 +9,7 @@
         :key="i"
       >
         <v-list-item-content>
-            <v-list-item-title @click="mentorshipPage(treino.link)">{{ treino.title }}</v-list-item-title>
+            <v-list-item-title @click="mentorshipPage(treino.link)">{{ formatName(treino.name) }}</v-list-item-title>
         </v-list-item-content>
       </v-list-item>
     </v-list-item-group>
@@ -18,23 +18,170 @@
 
 <script>
 export default {
-  props:{
-    treinos:{
-      type: Array
-    }
-  },
 
+  props:['mentorshipId', 'mentorshipWeekId'],
+  
   data(){
     return{
-      selectedItem:''
+      selectedItem:'',
+      treinos:[
+	{
+		"name": "treino_de_peito_e_tríceps (540p)",
+		"link": "809596214",
+		// "link": "https://vimeo.com/809596214",
+		"width": 480,
+		"height": 848,
+		"duration": 88,
+		"embed": {
+			"html": "<iframe src=\"https://player.vimeo.com/video/809596214?title=0&amp;byline=0&amp;portrait=0&amp;speed=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=263897\" width=\"480\" height=\"848\" frameborder=\"0\" allow=\"autoplay; fullscreen; picture-in-picture\" allowfullscreen title=\"treino_de_peito_e_tríceps (540p)\"></iframe>",
+			"badges": {
+				"hdr": false,
+				"live": {
+					"streaming": false,
+					"archived": false
+				},
+				"staff_pick": {
+					"normal": false,
+					"best_of_the_month": false,
+					"best_of_the_year": false,
+					"premiere": false
+				},
+				"vod": false,
+				"weekend_challenge": false
+			},
+			"buttons": {
+				"watchlater": true,
+				"share": true,
+				"embed": true,
+				"hd": false,
+				"fullscreen": true,
+				"scaling": true,
+				"like": true
+			},
+			"logos": {
+				"vimeo": true,
+				"custom": {
+					"active": false,
+					"use_link": false,
+					"sticky": false
+				}
+			},
+			"title": {
+				"name": "user",
+				"owner": "user",
+				"portrait": "user"
+			},
+			"end_screen": [],
+			"playbar": true,
+			"pip": true,
+			"autopip": true,
+			"volume": true,
+			"color": "00adef",
+			"colors": {
+				"color_one": "000000",
+				"color_two": "00adef",
+				"color_three": "ffffff",
+				"color_four": "000000"
+			},
+			"event_schedule": true,
+			"interactive": false,
+			"has_cards": false,
+			"outro_type": "videos",
+			"show_timezone": false,
+			"cards": [],
+			"speed": true
+		}
+	},
+	{
+		"name": "treino_de_ombro_e_abs (540p)",
+		"link": "809596068",
+		// "link": "https://vimeo.com/809596068",
+		"width": 480,
+		"height": 848,
+		"duration": 84,
+		"embed": {
+			"html": "<iframe src=\"https://player.vimeo.com/video/809596068?title=0&amp;byline=0&amp;portrait=0&amp;speed=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=263897\" width=\"480\" height=\"848\" frameborder=\"0\" allow=\"autoplay; fullscreen; picture-in-picture\" allowfullscreen title=\"treino_de_ombro_e_abs (540p)\"></iframe>",
+			"badges": {
+				"hdr": false,
+				"live": {
+					"streaming": false,
+					"archived": false
+				},
+				"staff_pick": {
+					"normal": false,
+					"best_of_the_month": false,
+					"best_of_the_year": false,
+					"premiere": false
+				},
+				"vod": false,
+				"weekend_challenge": false
+			},
+			"buttons": {
+				"watchlater": true,
+				"share": true,
+				"embed": true,
+				"hd": false,
+				"fullscreen": true,
+				"scaling": true,
+				"like": true
+			},
+			"logos": {
+				"vimeo": true,
+				"custom": {
+					"active": false,
+					"use_link": false,
+					"sticky": false
+				}
+			},
+			"title": {
+				"name": "user",
+				"owner": "user",
+				"portrait": "user"
+			},
+			"end_screen": [],
+			"playbar": true,
+			"pip": true,
+			"autopip": true,
+			"volume": true,
+			"color": "00adef",
+			"colors": {
+				"color_one": "000000",
+				"color_two": "00adef",
+				"color_three": "ffffff",
+				"color_four": "000000"
+			},
+			"event_schedule": true,
+			"interactive": false,
+			"has_cards": false,
+			"outro_type": "videos",
+			"show_timezone": false,
+			"cards": [],
+			"speed": true
+		}
+	}
+]
     }
   },
 
   methods:{
+    formatName(value){
+      let name =  value.split(" ")[0].replaceAll("_", " ")
+      name = `${name.charAt(0).toUpperCase() + name.slice(1)}`
+      return name
+    },
+    // async mentorshipVideo(){
+
+    // },
+
     mentorshipPage(link){
       this.$router.push(`/mentorship/${link}`)
     },
+  },
+
+  mounted(){
+    // this.mentorshipVideo(){}
   }
+  
 }
 </script>
 
