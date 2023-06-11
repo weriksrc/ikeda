0257dae38f4e8c1cea86d991f44b71498dab4606
weriks <ti15@viacometa.com.br>
2023-06-10 21:10:29 -0400

    refactor: ajusta grafico

--- src/views/user/evolution/components/BodyCompositionPieChart.vue
+++ src/views/user/evolution/components/BodyCompositionPieChart.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <v-card color="#232323">
     <BaseChart :chartOptions="lineChartOptions"></BaseChart>
-  </div>
+  </v-card>
 </template>
 
 <script>
@@ -22,18 +22,86 @@ export default {
     return {
       lineChartOptions: {
         chart: {
-          type: "line",
+          height: 350,
+          plotBackgroundColor: null,
+          plotBorderWidth: 0,
+          plotShadow: false,
+          backgroundColor: "#232323",
         },
         title: {
           text: "Composição Corporal",
+          style: {
+            color: "rgba(255,255,255, 0.3)"
+          }
         },
-        series: [
-          {
-            data: [1, 3, 2, 4, 5, 3, 2],
-          },
-        ],
+        legend: {
+          itemStyle: {
+            color: "rgba(255,255,255, 0.3)"
+          }
+        },
+        tooltip: {
+          pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        accessibility: {
+          point: {
+            valueSuffix: "%"
+          }
+        },
+        plotOptions: {
+          pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: false
+            },
+            showInLegend: true,
+            startAngle: 0,
+            endAngle: 360,
+            size: "100%"
+          }
+        },
+        series:[],
+        lang: {
+          noData: "Não há dados para serem apresentados"
+        },
+        noData: {
+          style: {
+            fontWeight: "normal",
+            fontSize: "1rem",
+            color: "rgba(0, 0, 0, 0.6)"
+          }
+        }
       },
     };
   },
+
+  watch:{
+    dataBodyComposition(value){
+      this.getValuesChart(value)
+    }
+  },
+
+  methods:{
+    getValuesChart(data){
+      const arrayData = [
+        { name: 'Gordura', y: data[2].body_fat_percentage },
+        { name: 'Massa Magra', y: data[2].lean_body_mass },
+        { name: 'Massa Gorda', y: data[2].fat_mass },
+        { name: 'Massa Muscular', y: data[2].muscle_mass },
+        { name: 'Massa Óssea', y: data[2].bone_mass },
+        { name: 'Massa Residual', y: data[2].residual_mass }
+      ];
+
+      this.lineChartOptions.series = {
+        type: "pie",
+        name: 'Percentual',
+        colorByPoint: true,
+        innerSize: "60%",
+        data: arrayData
+      };
+
+
+    }
+  }
 };
 </script>
