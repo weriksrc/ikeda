0e5d721bdac17888bf4346007eb8d000517b88a9
Weriks <ti15@viacometa.com.br>
2024-02-18 20:50:49 -0400

    refactor: remove funcao obsoleta

--- src/views/personal/users/components/UserCard.vue
+++ src/views/personal/users/components/UserCard.vue
@@ -1,63 +1,66 @@
 <template>
   <div>
-  <v-card class="user-card" @click="dialog = true">
-    <v-img src="../../../../assets/img/user-card.png">
-      <div class="user-information">
-        <v-avatar color="#777777" size="60" class="mt-2">
-          <v-icon dark>
-            mdi-account-circle
-          </v-icon>
-        </v-avatar>
-        <p class="ma-0 mt-2 subtitle-2">{{ formatName(name) }}</p>
-        <p :class="classAtivo(status)">{{ status ? 'Ativo' : 'Inativo' }}</p>
-      </div>
-    </v-img>
-  </v-card>
-  <ModalUser :dialog="dialog" :idUser="id" :name="name" @close="dialog = false" />
+    <v-card class="user-card" @click="dialog = true">
+      <v-img src="../../../../assets/img/user-card.png">
+        <div class="user-information">
+          <v-avatar color="#777777" size="60" class="mt-2">
+            <v-icon dark> mdi-account-circle </v-icon>
+          </v-avatar>
+          <p class="ma-0 mt-2 subtitle-2">{{ formatName(name) }}</p>
+          <p :class="classAtivo(status)">{{ status ? "Ativo" : "Inativo" }}</p>
+        </div>
+      </v-img>
+    </v-card>
+    <ModalUser
+      :dialog="dialog"
+      :idUser="id"
+      :name="name"
+      :status="status"
+      @close="dialog = false"
+    />
   </div>
 </template>
 
 <script>
-import  ModalUser from "@/views/personal/users/components/ModalUser.vue";
+import ModalUser from "@/views/personal/users/components/ModalUser.vue";
 export default {
-  props:["id","status","name"],
+  props: ["id", "status", "name"],
 
-  components:{
-    ModalUser
+  components: {
+    ModalUser,
   },
 
-  data(){
-    return{
-      dialog: false
-    }
+  data() {
+    return {
+      dialog: false,
+    };
   },
 
-  methods:{
-    formatName(name){
-      let arrayName = name.split(" ")
-      return arrayName[1] == 'de' ? `${arrayName[0]}` + ` ${arrayName[2]}` : `${arrayName[0]}` + ` ${arrayName[1] || ''} `
+  methods: {
+    formatName(name) {
+      let arrayName = name.split(" ");
+      return arrayName[1] == "de"
+        ? `${arrayName[0]}` + ` ${arrayName[2]}`
+        : `${arrayName[0]}` + ` ${arrayName[1] || ""} `;
     },
 
-    classAtivo(status){
-      return status ? 'ma-0 mt-2 subtitle-2 font-weight-bold green--text' :'ma-0 mt-2 subtitle-2 font-weight-bold red--text'
+    classAtivo(status) {
+      return status
+        ? "ma-0 mt-2 subtitle-2 font-weight-bold green--text"
+        : "ma-0 mt-2 subtitle-2 font-weight-bold red--text";
     },
-
-    teste(id){
-      console.log("id", id);
-    }
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
-.user-card{
+.user-card {
   border-radius: 10px;
 }
 
-.user-information{
+.user-information {
   position: absolute;
   text-align: center;
   width: 100%;
 }
-
 </style>
