633c3c5503d64be14c6185420eb554971258bd63
Weriks <ti15@viacometa.com.br>
2024-02-25 21:16:29 -0400

    refactor: remove comentario

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,11 +3,11 @@ import Personal from "@/views/personal/routes/index";
 import User from "@/views/user/routes/index";
 import Login from "@/views/login/routes/index";
 import Video from "@/views/video/routes/index";
+
 export default [
   {
     path: "/",
     name: "Home",
-    // redirect: "/evolution",
     component: System,
     meta: { isAuthenticated: true },
     children: [...Personal, ...User],
