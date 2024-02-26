f13c3c12b8f938a706b48f6bff1f70ac9dd896ed
Weriks <ti15@viacometa.com.br>
2024-02-25 21:20:23 -0400

    refactor: adiciona funcao para chamada openpix

--- src/views/user/profile/Index.vue
+++ src/views/user/profile/Index.vue
@@ -1,28 +1,44 @@
 <template>
   <v-row>
+    <v-col cols="6">
+      <v-card class="mx-3 my-4 px-3" @click="displayOpenPixModal()">Pix</v-card>
+    </v-col>
+    <v-col cols="6">
+      <v-card class="mx-3 my-4 px-3">Cartão</v-card>
+    </v-col>
     <v-col cols="12">
       <v-card class="mx-3 my-4 px-3 form-card">
-        <v-card-title class="subtitle-1 grey--text pb-0" color="#ffffff">Conta</v-card-title>
+        <v-card-title class="subtitle-1 grey--text pb-0" color="#ffffff"
+          >Conta</v-card-title
+        >
         <IdentificationForm />
-        <v-card-title class="subtitle-1 grey--text pb-0" color="#ffffff">Senha</v-card-title>
+        <v-card-title class="subtitle-1 grey--text pb-0" color="#ffffff"
+          >Senha</v-card-title
+        >
         <PasswordForm />
       </v-card>
     </v-col>
     <v-col cols="12">
       <v-card class="mx-3 my-4 px-3 form-card">
-        <v-card-title class="subtitle-1 grey--text pb-0" color="#ffffff">Composição Corporal</v-card-title>
+        <v-card-title class="subtitle-1 grey--text pb-0" color="#ffffff"
+          >Composição Corporal</v-card-title
+        >
         <BodyCompositionForm />
       </v-card>
     </v-col>
     <v-col cols="12">
       <v-card class="mx-3 my-4 px-3 form-card">
-        <v-card-title class="subtitle-1 grey--text pb-0" color="#ffffff">Dobras Cutaneas (mm)</v-card-title>
+        <v-card-title class="subtitle-1 grey--text pb-0" color="#ffffff"
+          >Dobras Cutaneas (mm)</v-card-title
+        >
         <DobrasCutaneas />
       </v-card>
     </v-col>
     <v-col cols="12">
       <v-card class="mx-3 my-4 px-3 form-card">
-        <v-card-title class="subtitle-1 grey--text pb-0" color="#ffffff">Circunferência (cm)</v-card-title>
+        <v-card-title class="subtitle-1 grey--text pb-0" color="#ffffff"
+          >Circunferência (cm)</v-card-title
+        >
         <CircumferencesForm />
       </v-card>
     </v-col>
@@ -35,20 +51,53 @@ import PasswordForm from "./components/PasswordForm";
 import BodyCompositionForm from "./components/BodyCompositionForm";
 import DobrasCutaneas from "./components/DobrasCutaneas";
 import CircumferencesForm from "./components/CircumferencesForm";
+import { mapGetters } from "vuex";
+
 export default {
-  components:{
+  components: {
     IdentificationForm,
     PasswordForm,
     BodyCompositionForm,
     DobrasCutaneas,
-    CircumferencesForm
-  }
+    CircumferencesForm,
+  },
 
-}
+  computed: {
+    ...mapGetters({
+      currentUser: "getUser",
+    }),
+  },
+
+  watch: {
+    currentUser(newVal, oldVal) {
+      console.log("getUser - watch", newVal);
+      // Execute qualquer lógica dependente dos dados aqui
+    },
+  },
+
+  methods: {
+    displayOpenPixModal() {
+      window.$openpix = window.$openpix || [];
+      window.$openpix.push([
+        "config",
+        { appID: process.env.VUE_APP_OPENPIX_API_APP_ID },
+      ]);
+
+      window.$openpix.push([
+        "pix",
+        {
+          value: 1000,
+          correlationID: this.currentUser.id,
+          description: "Mentoria",
+        },
+      ]);
+    },
+  },
+};
 </script>
 
 <style scoped>
-.form-card{
+.form-card {
   background-color: #232323;
   border-radius: 10px;
 }
