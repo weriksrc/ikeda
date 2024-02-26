aafed6dc36d49d2bc222ead5491cdfc4ab6c85e0
Weriks <ti15@viacometa.com.br>
2024-02-25 23:59:03 -0400

    refactor: ajuste visual nos cards de pagamento

--- src/views/user/profile/Index.vue
+++ src/views/user/profile/Index.vue
@@ -1,10 +1,36 @@
 <template>
-  <v-row>
-    <v-col cols="6">
-      <v-card class="mx-3 my-4 px-3" @click="displayOpenPixModal()">Pix</v-card>
+  <v-row class="background-profile">
+    <v-col cols="12" class="mt-4 px-3 py-0">
+      <v-card-title class="subtitle-1 grey--text py-0 pay-card" color="#ffffff"
+        >Pagamento</v-card-title
+      >
     </v-col>
-    <v-col cols="6">
-      <v-card class="mx-3 my-4 px-3">Cartão</v-card>
+    <v-col cols="6" class="pt-0">
+      <v-card
+        color="#232323"
+        height="130"
+        class="mx-3 mt-4 px-3"
+        @click="displayOpenPixModal()"
+      >
+        <div class="d-flex flex-column align-center pt-4">
+          <v-img src="@/assets/img/pix.svg" width="60"></v-img>
+          <v-card-title class="subtitle-1 grey--text pb-0" color="#ffffff"
+            >Pix</v-card-title
+          >
+        </div>
+      </v-card>
+    </v-col>
+    <v-col cols="6" class="pt-0">
+      <v-card color="#232323" height="130" class="mx-3 mt-4 px-3">
+        <div class="d-flex flex-column align-center pt-6">
+          <v-img src="@/assets/img/cartao.svg" width="70"></v-img>
+          <v-card-title
+            class="subtitle-1 grey--text pb-0 pay-card"
+            color="#ffffff"
+            >Cartão</v-card-title
+          >
+        </div>
+      </v-card>
     </v-col>
     <v-col cols="12">
       <v-card class="mx-3 my-4 px-3 form-card">
@@ -97,8 +123,16 @@ export default {
 </script>
 
 <style scoped>
+.background-profile {
+  background-color: #303030;
+}
+
 .form-card {
   background-color: #232323;
   border-radius: 10px;
+}
+
+.pay-card {
+  margin-top: 9px;
 }
 </style>
